1e4827a47f6f4a0e16de490e50f0b5e1e35083a5
Marvin Arlt <marvinarlt@gmx.net>
2024-10-17 00:40:43 +0200

    Finish parser implementation

--- parser.go
+++ parser.go
@@ -2,7 +2,9 @@ package query
 
 import (
 	"net/url"
+	"slices"
 	"strconv"
+	"strings"
 )
 
 const (
@@ -16,6 +18,9 @@ var (
 	ParamOffset = "offset"
 	ParamSelect = "select"
 	ParamSort   = "sort"
+
+	SeparatorField  = ","
+	SeparatorFilter = ":"
 )
 
 type Parser struct {
@@ -35,10 +40,107 @@ func NewParser(allowedFields []string) *Parser {
 }
 
 func (p *Parser) Parse(query url.Values) Query {
-	return newBuilder(p.allowedFields).
-		withLimit(p.parseLimit(query.Get(ParamLimit))).
-		withOffset(p.parseOffset(query.Get(ParamOffset))).
-		get()
+	return Query{
+		Limit:      p.parseLimit(query.Get(ParamLimit)),
+		Offset:     p.parseOffset(query.Get(ParamOffset)),
+		Select:     p.parseSelect(query.Get(ParamSelect)),
+		Sortings:   p.parseSort(query.Get(ParamSort)),
+		Filterings: p.parseFilter(query),
+	}
+}
+
+func (p *Parser) parseFilter(values url.Values) []Filtering {
+	filterings := make([]Filtering, 0, len(p.allowedFields))
+
+	for _, field := range p.allowedFields {
+		raw := values.Get(field)
+
+		if len(raw) == 0 {
+			continue
+		}
+
+		parts := p.splitClean(raw, SeparatorFilter, 2)
+
+		if len(parts) == 1 {
+			filterings = append(filterings, Filtering{
+				Field:  field,
+				Filter: FilterEquals,
+				Value:  parts[0], // TODO: Parse into correct type
+			})
+			continue
+		}
+
+		if !p.isAllowedFilterValue(parts[0]) {
+			continue
+		}
+
+		filterings = append(filterings, Filtering{
+			Field:  field,
+			Filter: parts[0],
+			Value:  parts[1],
+		})
+	}
+
+	return filterings
+}
+
+func (p *Parser) parseSort(raw string) []Sorting {
+	rawParts := p.splitClean(raw, SeparatorField, -1)
+	sortings := make([]Sorting, 0, len(rawParts))
+
+	for _, rawPart := range rawParts {
+		parts := p.splitClean(rawPart, SeparatorFilter, 2)
+
+		if len(parts) < 1 {
+			continue
+		}
+
+		if !p.isAllowedField(parts[0]) {
+			continue
+		}
+
+		if len(parts) < 2 {
+			sortings = append(sortings, Sorting{
+				Field: parts[0],
+				Order: OrderAsc,
+			})
+			continue
+		}
+
+		if !p.isAllowedOrderValue(parts[1]) {
+			continue
+		}
+
+		sortings = append(sortings, Sorting{
+			Field: parts[0],
+			Order: parts[1],
+		})
+	}
+
+	return sortings
+}
+
+func (p *Parser) parseSelect(raw string) []string {
+	fields := p.splitClean(raw, SeparatorField, -1)
+
+	return slices.DeleteFunc(fields, func(field string) bool {
+		return !p.isAllowedField(field)
+	})
+}
+
+func (p *Parser) splitClean(raw string, sep string, n int) []string {
+	rawParts := strings.SplitN(raw, sep, n)
+	parts := make([]string, 0, len(rawParts))
+
+	for _, rawPart := range rawParts {
+		part := strings.TrimSpace(rawPart)
+
+		if len(part) > 0 {
+			parts = append(parts, part)
+		}
+	}
+
+	return parts
 }
 
 func (p *Parser) parseLimit(raw string) int {
@@ -77,6 +179,18 @@ func (p *Parser) parseInt(raw string, fallback int) int {
 	}
 
 	return value
+}
+
+func (p *Parser) isAllowedField(field string) bool {
+	return slices.Contains(p.allowedFields, field)
+}
+
+func (p *Parser) isAllowedOrderValue(order string) bool {
+	return slices.Contains(allowedOrderValues, order)
+}
+
+func (p *Parser) isAllowedFilterValue(filter string) bool {
+	return slices.Contains(allowedFilterValues, filter)
 }
 
 func (p *Parser) WithMaxLimit(max int) *Parser {
